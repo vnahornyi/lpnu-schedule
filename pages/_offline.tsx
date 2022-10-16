b03ec7e8b4be411c64fb3e53134688fe5eccf8bf
Vladyslav Nahornyi <logan0mcv@gmail.com>
2022-10-17 02:30:51 +0300

    added offline page

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -1,7 +1,14 @@
+import { Container, Flex, Text } from '@chakra-ui/react';
 import { NextPage } from 'next';
 
 const OfflinePage: NextPage = () => {
-    return <div>Offline</div>;
+    return <Container maxW='container.xl'>
+        <Flex minH='100vh' justify='center' align='center'>
+            <Text fontWeight='bold' fontSize='3xl'>
+                Немає доступу до інтернету :(
+            </Text>
+        </Flex>
+    </Container>;
 }
 
 export default OfflinePage;
