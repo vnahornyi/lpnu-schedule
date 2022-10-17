a233564052666ab14efffeadce3d1ff4b3692184
Vladyslav Nahornyi <logan0mcv@gmail.com>
2022-10-17 11:52:34 +0300

    fix cache control

--- next.config.js
+++ next.config.js
@@ -9,4 +9,35 @@ const withPWA = require('next-pwa')({
 module.exports = withPWA({
 	reactStrictMode: true,
 	swcMinify: true,
+	async headers() {
+		return [
+			{
+				source: '/schedule/[group]',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 's-maxage=1, stale-while-revalidate=59'
+					}
+				]
+			},
+			{
+				source: '/settings',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 's-maxage=1, stale-while-revalidate=28799'
+					}
+				]
+			},
+			{
+				source: '/setup',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 's-maxage=1, stale-while-revalidate=28799'
+					}
+				]
+			}
+		]
+	}
 });
