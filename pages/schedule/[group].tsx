ee4f011eebf684b8da154b2765c377431da98a90
Vladyslav Nahornyi <logan0mcv@gmail.com>
2022-10-24 00:46:46 +0300

    hotfix date

--- pages/schedule/[group].tsx
+++ pages/schedule/[group].tsx
@@ -66,8 +66,8 @@ const SchedulePage: NextPage = () => {
         const currentDate = new Date();
         const days = [];
 
-        for (let i = 6; i >= 0; i--) {
-            days.push(currentDate.getDay() - i);
+        for (let i = 0; i < 7; i++) {
+            days.push(currentDate.getDay() === 0 ? 6 : (currentDate.getDay() - 1 - i));
         }
 
         return days.map(day => <FormatedTab key={day} day={day} />);
