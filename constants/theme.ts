9ecae71e809e59ef7706d6050a49c8cd619d1a6a
Vladyslav Nahornyi <logan0mcv@gmail.com>
2022-11-05 23:49:25 +0200

    fix

--- constants/theme.ts
+++ constants/theme.ts
@@ -2,21 +2,22 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
-  initialColorMode: 'light',
+  initialColorMode: 'system',
   useSystemColorMode: true,
 };
 
-const theme = extendTheme({ config, 
-  // styles: {
-  //   global: {
-  //     '@media (display-mode: standalone)': {
-  //       '*': {
-  //         userSelect: 'none',
-  //         WebkitTapHighlightColor: 'transparent'
-  //       }
-  //     }
-  //   }
-  // } 
+const theme = extendTheme({
+  config, 
+  styles: {
+    global: {
+      '@media (display-mode: standalone)': {
+        '*': {
+          userSelect: 'none',
+          WebkitTapHighlightColor: 'transparent'
+        }
+      }
+    }
+  } 
 });
 
 export default theme;
