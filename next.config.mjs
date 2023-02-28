debb9c33322f2eed7c52718d1c3054d7cf5ddfb0
vnahornyi <vnahornyi@outlook.com>
2023-02-28 17:30:04 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,56 +2,58 @@
 import nextPwa from 'next-pwa';
 
 const withPWA = nextPwa({
-	dest: 'public',
+    dest: 'public',
     register: true,
     skipWaiting: true,
-	disable: process.env.NODE_ENV === 'development'
+    publicExcludes: ['**/*'],
+    buildExcludes: [() => true],
+    disable: process.env.NODE_ENV === 'development',
 });
 
 export default withPWA({
-	reactStrictMode: true,
-	swcMinify: true,
-	webpack: config => {
-		config.resolve.fallback = {
-			...config.resolve.fallback,
-			net: false,
-			tls: false,
-			fs: false,
-			child_process: false,
-			perf_hooks: false
-		};
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack: config => {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            net: false,
+            tls: false,
+            fs: false,
+            child_process: false,
+            perf_hooks: false,
+        };
 
-		return config;
-	},
-	async headers() {
-		return [
-			{
-				source: '/schedule/[group]',
-				headers: [
-					{
-						key: 'Cache-Control',
-						value: 's-maxage=1, stale-while-revalidate=59'
-					}
-				]
-			},
-			{
-				source: '/settings',
-				headers: [
-					{
-						key: 'Cache-Control',
-						value: 's-maxage=1, stale-while-revalidate=28799'
-					}
-				]
-			},
-			{
-				source: '/setup',
-				headers: [
-					{
-						key: 'Cache-Control',
-						value: 's-maxage=1, stale-while-revalidate=28799'
-					}
-				]
-			}
-		]
-	}
+        return config;
+    },
+    async headers() {
+        return [
+            {
+                source: '/schedule/[group]',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 's-maxage=1, stale-while-revalidate=59',
+                    },
+                ],
+            },
+            {
+                source: '/settings',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 's-maxage=1, stale-while-revalidate=28799',
+                    },
+                ],
+            },
+            {
+                source: '/setup',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 's-maxage=1, stale-while-revalidate=28799',
+                    },
+                ],
+            },
+        ];
+    },
 });
